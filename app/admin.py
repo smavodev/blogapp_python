bcdab27989dcfa3076b88256163d85e2c6ead8c2
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-07-27 22:25:04 -0500

    Add Model Tag, Update Model Post, Renderizando Maquetado de la pagina post

--- app/admin.py
+++ app/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
+from app.models import Post, Tag
 
-from app.models import Post
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    list_display_links = ('title', 'slug')
+    list_filter = ('title', 'slug')
+
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    list_display_links = ('name', 'slug')
+    list_filter = ('name', 'slug')
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Tag, TagAdmin)
--- app/models.py
+++ app/models.py
@@ -1,4 +1,23 @@
 from django.db import models
+from django.utils.text import slugify
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        verbose_name = 'tags'
+        verbose_name_plural = 'tags'
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+        return super(Tag, self).save(*args,**kwargs)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -7,5 +26,8 @@ class Post(models.Model):
     last_updated = models.DateTimeField(auto_now=True)
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(null=True, blank=True, upload_to="images/")
+    tags = models.ManyToManyField(Tag, blank=True, related_name='post')
 
-
+    class Meta:
+        verbose_name = 'posts'
+        verbose_name_plural = 'posts'
