b9aa627cd0fe2926884034de385ddc7e820a9c25
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-07-27 20:41:55 -0500

    Add Templates, Add view (Index), Add Url, Add Css

--- app/views.py
+++ app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from app.models import Post
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'app/index.html')
+
+
+def post_page(request, slug):
+    post = Post.objects.get(slug=slug)
+    return render(request, 'app/post.html')
+
