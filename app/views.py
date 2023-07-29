fae652000a55e28d74353764b35f150889182baa
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2023-07-29 15:45:30 -0500

    Add Url, View, Template, Renderizando la pagina de Tags

--- app/views.py
+++ app/views.py
@@ -80,3 +80,19 @@ def post_page(request, slug):
     }
     return render(request, 'app/post.html', context)
 
+
+def tag_page(request, slug):
+    tag = Tag.objects.get(slug=slug)
+
+    top_posts = Post.objects.filter(tags__in=[tag.id]).order_by('-view_count')[0:2]
+    recent_posts = Post.objects.filter(tags__in=[tag.id]).order_by('-created_date')[0:2]
+
+    tags = Tag.objects.all()
+    context = {
+        'tag': tag,
+        'top_posts': top_posts,
+        'recent_posts': recent_posts,
+        'tags': tags
+    }
+    return render(request, 'app/tag.html', context)
+
